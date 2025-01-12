e5dfb1c68d58599649c4a7f5bb2b6e22d5bfb2ab
Ajay A Adsule <ajayadsule5@gmail.com>
2025-01-12 19:20:02 +0530

    config basecomponent for redirect and set http headers

--- client/todo/src/BaseComponent.tsx
+++ client/todo/src/BaseComponent.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { setAxiosAuthHeader } from "./lib/axios/axiosConfig";
+
 const BaseComponent = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAxiosAuthHeader(token);
+      navigate("/todo/home");
+    }
+  }, []);
+
   return <div>BaseComponent</div>;
 };
 
