60ac2a845994b813dae382146345c6375a0ab722
Ajay A Adsule <ajayadsule5@gmail.com>
2025-01-16 22:44:33 +0530

    add navbar list

--- client/todo/src/components/global/SideBar/AppSideBar.tsx
+++ client/todo/src/components/global/SideBar/AppSideBar.tsx
@@ -1,5 +1,19 @@
-import { Eye, Home } from "lucide-react";
+import {
+  BriefcaseBusiness,
+  CalendarClock,
+  CalendarFold,
+  ChevronDown,
+  Eye,
+  GraduationCap,
+  NotebookText,
+  Zap,
+} from "lucide-react";
 
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import {
   Sidebar,
   SidebarContent,
@@ -18,14 +32,47 @@ import TasklystLogo from "../TaskListLogo";
 // Menu items.
 const items = [
   {
-    title: "Home",
-    url: "/todo/home",
-    icon: Home,
-  },
-  {
     title: "Overview",
     url: "/todo/overview",
     icon: Eye,
+  },
+  {
+    title: "Today",
+    url: "/todo/today",
+    icon: Eye,
+  },
+  {
+    title: "Tomorrow",
+    url: "/todo/today",
+    icon: CalendarFold,
+  },
+  {
+    title: "Next 7 days",
+    url: "/todo/today",
+    icon: CalendarClock,
+  },
+];
+
+const listItems = [
+  {
+    title: "Work",
+    url: "/todo/work",
+    icon: BriefcaseBusiness,
+  },
+  {
+    title: "Workout",
+    url: "/todo/work",
+    icon: Zap,
+  },
+  {
+    title: "Learning",
+    url: "/todo/work",
+    icon: GraduationCap,
+  },
+  {
+    title: "Reading",
+    url: "/todo/work",
+    icon: NotebookText,
   },
 ];
 
@@ -33,11 +80,11 @@ export function AppSidebar() {
   return (
     <Sidebar className="shadow-xl">
       <SidebarHeader>
-        <TasklystLogo height="40" width="140" />
+        <TasklystLogo height="60" width="200" />
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Main</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
@@ -53,6 +100,32 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <Collapsible className="group/collapsible" defaultOpen>
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                List
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {listItems.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <NavLink to={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </NavLink>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
       </SidebarContent>
       <SidebarFooter>Footer</SidebarFooter>
     </Sidebar>
