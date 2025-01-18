862737a01f517a5c56930a7ba0c6ad4fc4d1731b
Ajay A Adsule <ajayadsule5@gmail.com>
2025-01-18 20:43:38 +0530

    make border perfect fit

--- client/todo/src/components/global/Task/TaskDetailTab.tsx
+++ client/todo/src/components/global/Task/TaskDetailTab.tsx
@@ -21,13 +21,13 @@ const TaskDetailTab = () => {
         />
       </div>
       <div className=" p-2">
-        <div className="border px-2 py-4  task_detail_main">
+        <div className="border border-primary/30 rounded-md  px-2 py-4  task_detail_main">
           <p className="text-xs text-foreground">My Work Task</p>
           <div className="task_details flex flex-col gap-y-1 mt-2 font-semibold">
-            <h2>{task?.title}</h2>
+            <h2 className="text-xl">{task?.title}</h2>
             <TaskDescription description={task.description} />
           </div>
-          <div className="mt-6 flex flex-col gap-y-2  ">
+          <div className="mt-6 flex flex-col gap-y-4  ">
             <TaskInfo label="Status" value={task?.status} />
             <TaskInfo
               label="Date"
